a681a06a199a41469d8caf6b41dbc88d0426e246
Kostia Ohulchanskyi <k.ohulchanskyi@gmail.com>
2025-02-27 14:16:35 +0000

    cleanup

--- src/Monads/Monads/Core/IMonad.cs
+++ src/Monads/Monads/Core/IMonad.cs
@@ -1,14 +1,3 @@
 ﻿namespace Monads.Core;
 
-public interface IMonad
-{
-    IMonad<TMap> Bind<TMap>(Func<object?, IMonad<TMap>> map);
-}
-
-public interface IMonad<out T> : IMonad
-{
-    IMonad<TMap> IMonad.Bind<TMap>(Func<object?, IMonad<TMap>> map) =>
-        Bind<TMap>(x => map(x));
-
-    IMonad<TMap> Bind<TMap>(Func<T, IMonad<TMap>> map);
-}
+public interface IMonad<T>;
